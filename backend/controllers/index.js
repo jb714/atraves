08e466c5c4fc8a8abf8cdb8560c5c36bcc221957
Justin Bell <justinbell714@gmail.com>
2017-07-17 17:55:25 -0700

    Began Postgres migration

---
+++ backend/controllers/index.js
@@ -0,0 +1,7 @@
+var messageBoxesCtrl = require('./messageBoxesCtrl');
+var messagesCtrl = require('./messagesCtrl');
+
+module.exports = {
+  messageBoxesCtrl,
+  messagesCtrl
+};
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express   = require('express');
 // var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var manager = require('./app/Messages/DB/manager');
+var messageBoxesCtrl = require('./backend/controllers/').messageBoxesCtrl;
+var messagesCtrl = require('./backend/controllers/').messagesCtrl;
 var app = express();
 
 //Express Logic
@@ -12,14 +14,14 @@ app.use(express.static(__dirname));
 
 app.listen(port);
 
-app.post('/messages', manager.postMessage)
-app.get('/messages', manager.getMessages)
+app.post('/messages', messageBoxesCtrl.createBox)
+// app.get('/messages', manager.getMessages)
 
 console.log("The music is playing on port " + port);
 
 
 //DB Logic
-var dbURI='postgres://hygnvykngwauoi:fce9f2a4cf075c522aa4c5f8e9c27c08de4a050ff9b4364485617ddc4808c7d2@ec2-23-23-248-162.compute-1.amazonaws.com:5432/d8pt31113jj09l'
+var dbURI='postgres://hygnvykngwauoi:fce9f2a4cf075c522aa4c5f8e9c27c08de4a050ff9b4364485617ddc4808c7d2@ec2-23-23-248-162.compute-1.amazonaws.com:5432/d8pt31113jj09l';
 // mongoose.connect(dbURI);
 //
 // mongoose.connection.on('connected', function(){console.log("Mongoose. We're up")})
