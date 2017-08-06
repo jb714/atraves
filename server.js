79cab1adff6d15a28b4561229854796c77fb1497
Justin Bell <justinbell714@gmail.com>
2017-08-06 11:21:16 -0700

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express   = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var manager = require('./backend/DB/manager');
-var config = require('./config.js')
+// var config = require('./config.js')
 var app = express();
 
 //Express Logic
